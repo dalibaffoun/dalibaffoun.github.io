d9d773c9a52308d4a51eeaecddc1545f0c725b54
dalibaffoun <mabaffoun@gmail.com>
2020-02-25 13:49:46 +0100

    deuxieme init

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,27 @@
 setTimeout(function() {
-var txt;
-var dial = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please en cliquant sur OK");
-if( dial == true ) {
-  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-  return true;
-}
+    var txt;
+    var dial = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please en cliquant sur OK");
+    if (dial == true) {
+        window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        return true;
+    }
 
 }, 10000);
 
 
-function psychedelicMode(){
+function psychedelicMode() {
 
-  document.querySelectorAll('p').forEach(e => e.style.color = "blue");
-  document.querySelectorAll('p').forEach(e => e.style.fontFamily = "papyrus");
-  document.querySelectorAll('h1', 'h2', 'h3').forEach(e => e.style.color = "green");
-  document.querySelectorAll('h1', 'h2', 'h3').forEach(e => e.style.fontFamily = "Comic Sans");
+    document.querySelectorAll('p').forEach(e => e.style.color = "blue");
+    document.querySelectorAll('p').forEach(e => e.style.fontFamily = "papyrus");
+    document.querySelectorAll('h1', 'h2', 'h3').forEach(e => e.style.color = "green");
+    document.querySelectorAll('h1', 'h2', 'h3').forEach(e => e.style.fontFamily = "Comic Sans");
 
-  document.documentElement.style.backgroundColor = "magenta";
+    document.documentElement.style.backgroundColor = "magenta";
 };
+
+const myButton = document.querySelector('#mybutton');
+console.log(myButton);
+
+myButton.addEventListener("click", function() {
+    psychedelicMode();
+})
